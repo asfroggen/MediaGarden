ef07ea4ddf4f1d4f478122255ab5913435c0f73f
Esau <yosoypawn@hotmail.com>
2022-08-30 19:23:13 -0500

    main viewmodel

--- app/src/main/java/com/esaudev/mediagarden/di/AppModule.kt
+++ app/src/main/java/com/esaudev/mediagarden/di/AppModule.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.esaudev.mediagarden.R
+import com.esaudev.mediagarden.exoplayer.MusicServiceConnection
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -26,5 +27,11 @@ object AppModule {
             .error(R.drawable.ic_image)
             .diskCacheStrategy(DiskCacheStrategy.DATA)
     )
+
+    @Singleton
+    @Provides
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context
+    ) = MusicServiceConnection(context)
 
 }
